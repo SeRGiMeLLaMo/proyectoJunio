c0388acbd2edecae3804fbb2d6916cbac5eef04f
Sergio Cubero <scubcar521@gmail.es>
2023-06-02 09:20:06 +0200

    cambios

---
+++ src/main/java/com/example/sexxop/MensajeSignupController.java
@@ -0,0 +1,13 @@
+package com.example.sexxop;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class MensajeSignupController {
+
+    @FXML
+    public void buttOk1() throws IOException {
+        HelloApplication.setRoot("login");
+    }
+}
